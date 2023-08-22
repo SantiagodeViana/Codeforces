74470560c858a71230908c6dea3a5de85d3e4c05
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-08-22 03:32:45 +0200

    Add files via upload

---
+++ Educational Codeforces Round 15/PROBLEM - B.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int size;
+	cin >> size;
+	int array [size];
+	for (int i = 0; i < size; i++){
+        cin >> array [i];
+    }
+	int a, b, answer;
+	int min;
+	min = 0;
+	for (int i = 0; min < size; i++){
+        array [min] = a;
+        int power;
+        power = a + array [i]/ 2);
+        if power % 2 == 0}
+           answer += 1;
+        }
+        if (i < size){
+              min += 1;
+        }                             
+	cout << answer;
+	return 0;
+}
