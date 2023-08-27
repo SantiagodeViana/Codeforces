f839a6fd295d7dc9bda78623b243d5dbf8438953
Santiago Viana <68198881+SantiagodeViana@users.noreply.github.com>
2023-08-27 14:20:09 +0200

    Add files via upload

---
+++ Educational Codeforces Round 16/Magic Odd Square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int matrix [48][48];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        
+        }
+    for (int i = 0; i < n; i++){
+             
+        }
+    return 0;
+}
